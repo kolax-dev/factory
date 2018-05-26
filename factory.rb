d3b5fb57b6627023035b9cb5c06b00548ed7c3af
kolax <kolax@kolax.com>
2018-05-26 18:39:03 +0300

    new pull request

--- factory.rb
+++ factory.rb
@@ -1,32 +1,32 @@
-class Factory 
+class Factory
 
 	include Enumerable
-	
-	 def self.new(*args, &block) 
-	 	
-	 	class_name = args.shift  if (args[0].is_a?(String) && args[0].match(/[A-Z]/))
-         
-		obj = Class.new  do 	
 
-			attr_accessor :vars	
+	 def self.new(*args, &block)
+
+	 	class_name = args.shift  if (args[0].is_a?(String) && args[0].match(/[A-Z]/))
+
+		obj = Class.new  do
+
+			attr_accessor :vars
 
 			define_method :initialize do |*vars|
 				raise "ArgumentError" unless vars.count == args.count
 				@args = args
-				@vars = vars															
-			end	
-			
-		    class_eval(&block) if block
+				@vars = vars
+			end
+
+		  class_eval(&block) if block
 
 			args.each_with_index do |item, index|
 				define_method item do
-					@vars[index]	
-				end				
-			end			
+					@vars[index]
+				end
+			end
 
-		    define_method :== do |other|
+		  define_method :== do |other|
 			    if (other.is_a? obj) && (@vars == other.vars)
-			      true 			    	     
+			      true
 			    else
 			      false
 			    end
@@ -34,41 +34,41 @@ class Factory
 
 			alias :eql? :==
 
-			def each 
+			def each
 				  if block_given?
 				    yield(@vars)
 				  else
 				     'no block'
 				  end
-			end	
+			end
 
-			def to_a 
-				@vars		
+			def to_a
+				@vars
 			end
 
 			alias :values :to_a
 
 			def values_at *items
 				arr = []
-				items.each do |item|	
-					arr << @vars[item] 
-				end	
-				arr	
+				items.each do |item|
+					arr << @vars[item]
+				end
+				arr
 			end
 
 			def to_h
 				hash = {}
-				@args.each_with_index do |item, index|	
-					hash[item] = @vars[index]	
-				end	
-				hash		
+				@args.each_with_index do |item, index|
+					hash[item] = @vars[index]
+				end
+				hash
 			end
 
 			def dig (*items)
-				to_h.dig(*items)					
+				to_h.dig(*items)
 			end
 
-			def to_s 
+			def to_s
 				str = to_h.collect{ |k, v| "#{k}='#{v}'" }.join(', ')
                "#<factory #{self.class.name} #{str}>"
 			end
@@ -76,64 +76,63 @@ class Factory
 			alias :inspect  :to_s
 
 			def [] (num)
-				if num.is_a? Numeric				
+				if num.is_a? Numeric
 				     @vars[num]
 				else
-					@args.each_with_index do |item, index|	
-				    	return @vars[index]  if num.to_s == item.to_s														
-					end	
-				end			
-			end					
-
-			def []= (num,var)
-				if num.is_a? Numeric				
-				     @vars[num] = var
-				else
-					@args.each_with_index do |item, index|	
-				    	return @vars[index] = var  if num.to_s == item.to_s														
-					end	
-				end			
+					@args.each_with_index do |item, index|
+				    	return @vars[index]  if num.to_s == item.to_s
+					end
+				end
 			end
 
-			def each_pair 				 
-				  if block_given?				  	
-				  	@args.each_with_index do |item, index|						
+			def []= (num,var)
+				if num.is_a? Numeric
+				     @vars[num] = var
+				else
+					@args.each_with_index do |item, index|
+				    	return @vars[index] = var  if num.to_s == item.to_s
+					end
+				end
+			end
+
+			def each_pair
+				  if block_given?
+				  	@args.each_with_index do |item, index|
 						yield(item,@vars[index])
-					end	 
+					end
 				  else
 				    puts 'no block'
 				  end
 			end
-			def select 							 
-				  if block_given?	
-				  	arr = []			  	
-					@vars.each do |item|	
-						arr.push(item) if yield(item)											
-					end 
+			def select
+				  if block_given?
+				  	arr = []
+					@vars.each do |item|
+						arr.push(item) if yield(item)
+					end
 					arr
 				  else
 				    @vars.each.class
 				  end
 			end
-			
+
 			def length
 				@vars.count
-			end	
+			end
 
 			alias :size :length
 
-			def members 
+			def members
 				arr = []
-				@args.each_with_index do |item|						
+				@args.each_with_index do |item|
 					arr.push(item)
-				end	
-			end					
-									
+				end
+			end
+
 		end
 
 		class_name ? const_set(class_name, obj) : obj
-				
+
 	 end
 
 end
-
